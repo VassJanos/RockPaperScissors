9d8e020e8a4592d1ea013dfb971119c0b44619d5
Vass János <vassjanos.prog@gmail.com>
2025-04-16 00:13:11 +0200

    Did some functions
    I made two functions, one for the player's choice and another for the computer's.
    I'am too tired to write this commit message in details..

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
-console.log("hello world!!");
+let humanScore = 0;
+let compScore = 0;
+
+function GetComputerChoice() {
+    let choice = " ";
+
+    let rand = Math.floor(Math.random()*3+1);
+
+    if (rand == 1) {
+        choice = "rock";
+    } else if (rand == 2) {
+        choice = "paper";
+    } else {
+        choice = "scissors";
+    }
+    
+    return (choice);
+}
+
+function GetHumanChoice() {
+    let choice = prompt("choose your destiny:").toLowerCase();
+    return choice;
+}
+
+function playRound(GetComputerChoice, GetHumanChoice) {
+    
+}
+
+/*
+GetComputerChoice (math.random)
+
+GetHumanChoice (prompt)
+-make it case insensitive
+
+global scope var - humanScore CompScore
+
+playRound (parameters humanChoice:compChoice)
+-representing round winner exm.: "You lose! Paper beats Rock”
+
+playGame
+-calls playRound for 5 rounds
+*/
